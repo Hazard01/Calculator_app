6193dd854cdae846d26d5cf62c1cb57b6bdf3641
Hazard01 <amang663@gmail.com>
2024-04-11 20:51:13 +0530

    commit

---
+++ script.js
@@ -0,0 +1,61 @@
+
+errorMsg = '';
+result = 0;
+isValidate= false;
+expression = '';
+
+function appendNum(number) {
+    document.getElementById('errorMsg').innerHTML = "";
+    if(expression == 'Infinity' || expression == 'Error' || expression == '0'){
+        expression = '';
+    }
+    if(expression.startsWith('+') || expression.startsWith('*') || expression.startsWith('/') || expression.startsWith('0')){
+        expression = ''
+    }
+    console.log(number);
+    expression += number;
+    document.getElementById('input').value = expression;
+}
+
+function appendOperator(operator) {
+    document.getElementById('errorMsg').innerHTML = "";
+    if(expression.endsWith('+') || expression.endsWith('*') || expression.endsWith('/') || expression.endsWith('.') || expression.endsWith('-')){
+        expression = expression.slice(0,expression.length-1) + operator;
+    }else{
+        expression += operator;
+    }
+    document.getElementById('input').value = expression;
+}
+
+function equal() {
+    try {
+        if (validateInput()) {
+            const result = eval(this.expression);
+            document.getElementById('display').value = result;
+            this.expression = result.toString();
+        }
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+        this.expression = '';
+    }
+}
+
+function validateInput() {
+    if (expression.endsWith('+') || expression.endsWith('*') || expression.endsWith('/') || expression.endsWith('.') || expression.endsWith('-')) {
+        document.getElementById('errorMsg').innerHTML = "Please enter the valid number"; 
+        alert("Enter valid number");
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function del() {
+    this.expression = this.expression.slice(0, -1);
+    document.getElementById('input').value = this.expression;
+}
+
+function reset() {
+    this.expression = '';
+    document.getElementById('input').value = this.expression;
+}
